1fa1a78cfb1ecaa69a2cf5cb317423fe3e013c7a
BrianC0des <bryansaavedrab272@gmail.com>
2026-03-19 13:02:00 +0800

    change org fees to manage fees

--- assets/scripts/sidebar-template.js
+++ assets/scripts/sidebar-template.js
@@ -28,7 +28,7 @@ const sidebarHTML = `
             </div>
             <ul class="sub-menu">
             <li><a class="link_name" href="#">Payments</a></li>
-             <li><a href="../organization/add-payment.html">Org Fees</a></li>
+             <li><a href="../organization/add-payment.html">Manage Fees</a></li>
             <li><a href="../organization/payment-history.html">Payment History</a></li>     
             </ul>
         </li>
